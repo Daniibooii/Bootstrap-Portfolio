6b15116eb1e42374bb42e18cd4bfbe3e705af61c
unknown <gw-danmace@hotmail.com>
2018-04-07 15:25:22 -0400

    closed some <img> tags, switched order or buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,17 @@
 $("#about-image2").hide();
 $("#email-address-image").hide();
 
+/*var clickDisabled = false;
+
+function clickDisabler(){
+   clickDisabled = true;
+   setTimeout(clickEnabler, 3000)
+   };
+
+ function clickEnabler(){
+   clickDisabled = false;
+   };
+*/
 $(document).ready(function(){
 
 function revealDoggo (){
@@ -14,6 +25,7 @@ $("#email-address-image").show();
 $("#about-image1").click(function(){
   $("#about-image1").hide();
   revealDoggo();
+//  clickDisabler();
 });
 
 $(".icon2").click(function(){
